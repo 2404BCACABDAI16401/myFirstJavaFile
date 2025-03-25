7b7486d338c822c04675b661a81294d7c627c28f
2404BCACABDAI16401 <aditidwivedi73@gmail.com>
2025-03-25 23:46:52 +0530

    added one more file

---
+++ choice.java
@@ -0,0 +1,28 @@
+public class choice
+{
+ public static void main(string argsp[])
+ { 
+   int n = 3;
+   system.out.println("enter a choice \n, 1to4");
+   swtivh(n)
+{
+case1:
+     system.out.println("one");
+    break;
+
+case2:
+     system.out.println("two");
+    break;
+
+case3:
+     system.out.println("three");
+    break;
+
+case4:
+     system.out.println("four");
+    break;
+    default
+       system.out.println("default case \n");
+     }
+    }
+   }
